fa8a3c8061ed2a59c073d4bbaeb322f4b25c6741
Micah Cooper and Rye Mason <dev+mrmicahcooper+ryenotbread@hashrocket.com>
2013-02-08 13:33:19 -0500

    Add rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Marketem::Application.config.secret_token = '06c196f1b5b377264b7ebe8d2fce171a5aa572db54c0ce6026f385c0664cb2da65795c3566b58537bebdfb787c5aa93d37bf86ef7aae8a2478ba4400d6a996f3'
